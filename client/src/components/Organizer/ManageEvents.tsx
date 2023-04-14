ac9ce78ad1591dce393c8c687b609a4d453a9ca1
Satsuki884 <stasyaklim884@gmail.com>
2023-04-14 21:06:32 +0300

    events manage

--- client/src/components/Organizer/ManageEvents.tsx
+++ client/src/components/Organizer/ManageEvents.tsx
@@ -1,22 +1,36 @@
 import Sidebar2 from "../sidebar/sidebar2"
 import Header from "../sidebar/Header"
 import AllEvents from "../Admin/Events/AllEvents"
+import "../Admin/styles/styleAdminPanelMain.scss";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import "./style.scss";
 
 export function ManageEvents() {
 
     return (
-        <div className="organizer-manage-events-page">
-
+        <div className="admin-panel">
             <Sidebar2 />
-            <div className="organizer-page">
+            <div className="admin-page">
                 <Header />
-                <div className="organizer-panel-content">
-                    <AllEvents />
-                </div>
-            </div>
+                <div className="admin-panel-content">
+                    {/* <ManageButtons setSave={setSave} /> */}
+                    <div className="lists-contaner">
+                        <Tabs>
+                            <TabList>
+                                <Tab>Events</Tab>
+                            </TabList>
 
+                            <TabPanel className="panel">
+                                <AllEvents />
+                            </TabPanel>
+
+                        </Tabs>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
